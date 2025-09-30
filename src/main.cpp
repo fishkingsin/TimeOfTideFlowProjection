b9428bbbff9238ac04081d101143a51a27365b0a
James Kong <fishkingsin@gmail.com>
2025-10-01 00:00:49 +0800

    [chore] update window setting

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,13 @@ int main() {
 #ifdef USE_PROGRAMMABLE_GL
 	windowSettings.setGLVersion(4, 1);
 #endif
-	windowSettings.setSize(1280, 720);
+	windowSettings.setSize(1920, 1080);
 	windowSettings.windowMode = OF_WINDOW;
-
+#ifdef DEBUG
+	windowSettings.decorated = true;
+#else
+	windowSettings.decorated = false;
+#endif
 	ofCreateWindow(windowSettings);
 
 	ofRunAppWithAppUtils(new ofApp());
